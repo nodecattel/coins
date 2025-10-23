dee32e26392e0fbbd5d8ecbb7d0265c23ecccd9a
Flow <cipianon@gmail.com>
2025-10-23 13:14:22 +0000

    add Base chain

--- utils/generate_app_configs.py
+++ utils/generate_app_configs.py
@@ -149,6 +149,7 @@ class CoinConfig:
             "ETC": "Ethereum Classic",
             "ETH": "ERC-20",
             "ETH-ARB20": "Arbitrum",
+            "ETH-BASE": "Base",
             "EWT": "EWT",
             "FTM": "FTM-20",
             "GLMR": "Moonbeam",
